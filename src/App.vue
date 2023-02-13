a4ff049355fe138572754339e102667455198613
AlbineM <misery.albibne@gmail.com>
2023-02-13 06:59:59 +0100

    [INIT] EDIT - Moved Header/Footer to App.vue comp

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TheFooter from '@/components/TheFooter.vue';
+import TheHeader from '@/components/TheHeader.vue';
 </script>
 
 <template>
+  <TheHeader />
   <RouterView />
+  <TheFooter />
 </template>
 
