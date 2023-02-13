f6c4e3c2fea2df9fbc350e97a4e86ad8394a4e31
AlbineM <misery.albibne@gmail.com>
2023-02-13 07:02:40 +0100

    [FEATURE] ADD menu router links

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -25,9 +25,9 @@
             </div>
             <div class="hidden sm:block sm:ml-6">
               <div class="flex space-x-4">
-                <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                <router-link :to="{name: 'home'}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</router-link>
 
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Restaurants List</a>
+                <router-link :to="{name: 'restaurants'}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Restaurants List</router-link>
               </div>
             </div>
           </div>
@@ -36,9 +36,9 @@
 
       <div class="sm:hidden" id="mobile-menu">
         <div class="px-2 pt-2 pb-3 space-y-1">
-          <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</a>
+          <router-link :to="{name: 'home'}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</router-link>
 
-          <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Restaurants List</a>
+          <router-link :to="{name: 'restaurants'}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Restaurants List</router-link>
         </div>
       </div>
     </nav>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-import TheHeader from '@/components/TheHeader.vue'
-import TheFooter from '@/components/TheFooter.vue'
+import TheWelcome from '@/components/TheWelcome.vue';
+
 </script>
 
 <template>
-  <TheHeader />
   <!-- The Container -->
   <div class="mx-52">
     <TheWelcome />
   </div>
-  <TheFooter />
+
 </template>
